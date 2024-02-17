c0b447b2e8a61a30f1cb94776630538863ceddcf
Desmondxx2000 <157266806+Desmondxx2000@users.noreply.github.com>
2024-02-16 19:00:27 -0500

    Add files via upload

--- asset-v1/app.js
+++ asset-v1/app.js
@@ -1,2 +1,106 @@
-/* 0.5.1 2020-12-04 18:40:10 */
-var app=new function(){this.name="Evadare",this.version="8",this.date="2021",this.folder="asset-v5/",this.looptime=6857,this.bpm=140,this.totalframe=328,this.nbpolo=7,this.nbloopbonus=1,this.bonusloopA=!0,this.bonusendloopA=!1,this.recmaxloop=23,this.recminloop=3,this.recmintime=Math.round(this.looptime/1e3)*this.recminloop,this.spritepolo="polo-sprite.png",this.spritepicto="game-picto.png",this.colBck="#0a190f",this.col0="#502f7c",this.col1="#502f7c",this.col2="#3b225c",this.col3="#2c1a45",this.col4="#251739",this.animearray=[{name:"1_Lanturi",color:"482878",uniqsnd:!0},{name:"2_JackO",color:"482878",uniqsnd:!0},{name:"3_Kra",color:"482878",uniqsnd:!0},{name:"4_Kah",color:"482878",uniqsnd:!0},{name:"5_Pscs",color:"482878",uniqsnd:!0},{name:"6_Bass",color:"880808",uniqsnd:!1},{name:"7_Alien",color:"880808",uniqsnd:!1},{name:"8_Piano",color:"880808",uniqsnd:!1},{name:"9_Brass",color:"880808",uniqsnd:!1},{name:"11_Slopot",color:"299100",uniqsnd:!0}],this.bonusarray=[{name:"sorry",src:"v5-b1-felicidade-hb.mp4",code:"1,2,6,8,10",sound:"bonus-felicidade",aspire:"aspire"}],this.unlockerarray=[];for(var n=0,o=this.animearray.length;n<o;n++){var i=this.animearray[n].name;this.animearray[n].soundA=i+"_a",this.animearray[n].soundB=this.animearray[n].uniqsnd?i+"_a":i+"_b",this.animearray[n].anime=i+"-sprite.png",this.animearray[n].animeData=i+".json"}};
+/* 0.5.4 2021-04-23 21:06:13 */
+var app = new function() {
+    this.name = "Alpha", this.version = "1", this.date = "2009", this.folder = "asset-v1/", this.looptime = 5333, this.bpm = 90, this.totalframe = 260, this.nbpolo = 7, this.nbloopbonus = 2, this.bonusloopA = !1, this.bonusendloopA = !1, this.recmaxloop = 34, this.recminloop = 4, this.recmintime = Math.round(this.looptime / 1e3) * this.recminloop, this.spritepolo = "polo-sprite.png", this.spritepicto = "game-picto.png", this.colBck = "#121212", this.col0 = "#919191", this.col1 = "#5f5f5f", this.col2 = "#4b4b4b", this.col3 = "#373737", this.col4 = "#232323", this.animearray = [{
+        name: "1_lead",
+        color: "828282",
+        uniqsnd: !0
+    }, {
+        name: "2_deux",
+        color: "828282",
+        uniqsnd: !0
+    }, {
+        name: "3_kosh",
+        color: "828282",
+        uniqsnd: !0
+    }, {
+        name: "4_shpok",
+        color: "828282",
+        uniqsnd: !0
+    }, {
+        name: "5_tom",
+        color: "828282",
+        uniqsnd: !0
+    }, {
+        name: "6_nouana",
+        color: "828282",
+        uniqsnd: !0
+    }, {
+        name: "7_scratch",
+        color: "828282",
+        uniqsnd: !0
+    }, {
+        name: "8_trill",
+        color: "828282",
+        uniqsnd: !0
+    }, {
+        name: "9_bass",
+        color: "828282",
+        uniqsnd: !0
+    }, {
+        name: "10_uh",
+        color: "828282",
+        uniqsnd: !0
+    }, {
+        name: "11_nugu",
+        color: "828282",
+        uniqsnd: !0
+    }, {
+        name: "12_guit",
+        color: "828282",
+        uniqsnd: !0
+    }, {
+        name: "13_tromp",
+        color: "828282",
+        uniqsnd: !0
+    }, {
+        name: "14_pouin",
+        color: "828282",
+        uniqsnd: !0
+    }, {
+        name: "15_tung",
+        color: "828282",
+        uniqsnd: !0
+    }, {
+        name: "16_aoun",
+        color: "828282",
+        uniqsnd: !0
+    }, {
+        name: "17_hum",
+        color: "828282",
+        uniqsnd: !0
+    }, {
+        name: "18_get",
+        color: "828282",
+        uniqsnd: !0
+    }, {
+        name: "19_tellme",
+        color: "828282",
+        uniqsnd: !0
+    }, {
+        name: "20_make",
+        color: "828282",
+        uniqsnd: !0
+    }], this.bonusarray = [{
+        name: "The Choir",
+        src: "v1-b1-choir-hb.mp4",
+        code: "2,4,8,11,15",
+        sound: "bonus-choir",
+        aspire: "aspire"
+    }, {
+        name: "The Sailor",
+        src: "v1-b2-sailor-hb.mp4",
+        code: "10,11,14,15,20",
+        sound: "bonus-sailor",
+        aspire: "aspire-sailor"
+    }, {
+        name: "Santa",
+        src: "v1-b3-santa-hb.mp4",
+        code: "1,9,13,14,16",
+        sound: "bonus-santa",
+        aspire: "aspire-santa"
+    }];
+    for (var n = 0, o = this.animearray.length; n < o; n++) {
+        var a = this.animearray[n].name;
+        this.animearray[n].soundA = a + "_a", this.animearray[n].soundB = this.animearray[n].uniqsnd ? a + "_a" : a + "_b", this.animearray[n].anime = a + "-sprite.png", this.animearray[n].animeData = a + ".json"
+    }
+};
